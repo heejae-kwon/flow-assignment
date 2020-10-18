4d0265e355b78e1f0ebed20634745fb4f55ba576
Heejae Kwon <hjkwon93@kakao.com>
2020-10-18 16:01:03 +0900

    Updates

---
+++ precache-manifest.01d7a0856faf6134199ff31b4a311c27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4746fd806d262a56e057e8fcf52e0118",
+    "url": "/flow-assignment/index.html"
+  },
+  {
+    "revision": "96f8acdb3993d6de1feb",
+    "url": "/flow-assignment/static/js/2.75f69077.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/flow-assignment/static/js/2.75f69077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8da4ca6721b721dd5f9",
+    "url": "/flow-assignment/static/js/main.447cafb4.chunk.js"
+  },
+  {
+    "revision": "1704f4212628b0ded50e",
+    "url": "/flow-assignment/static/js/runtime-main.cc9c39f2.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/flow-assignment/precache-manifest.01d7a0856faf6134199ff31b4a311c27.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/flow-assignment/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
